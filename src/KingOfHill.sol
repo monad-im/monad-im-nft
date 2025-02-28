e6d434c00b40d9ff8c6c359c7465ad31f2b4bf70
mike <jfkz@users.noreply.github.com>
2025-02-28 01:50:12 +0100

    feat: updates

--- src/KingOfHill.sol
+++ src/KingOfHill.sol
@@ -6,16 +6,36 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract KingOfHill is ERC721, Ownable {
     uint256 private _tokenIdCounter;
-    mapping(address => bool) private _hasMinted; // Tracks if an address has minted an NFT
+    mapping(address => bool) private _hasMinted; // Tracks if an address has an NFT
 
-    constructor() ERC721("KingOfHill", "KOH") {}
+    // Pass the token name and symbol to the ERC721 constructor
+    constructor() ERC721("KingOfHill", "KOH") Ownable() {}
 
     function safeMint(address to) public onlyOwner {
         require(!_hasMinted[to], "KingOfHill: Each address can hold only one NFT");
-        _hasMinted[to] = true; // Mark the address as having minted an NFT
+        _hasMinted[to] = true; // Mark the address as having an NFT
 
         uint256 tokenId = _tokenIdCounter;
         _tokenIdCounter += 1;
         _safeMint(to, tokenId);
+    }
+
+    // Override _beforeTokenTransfer to enforce the "one NFT per wallet" rule
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal virtual override {
+        super._beforeTokenTransfer(from, to, tokenId);
+
+        // Skip check for minting (from == address(0)) and burning (to == address(0))
+        if (from != address(0)) {
+            _hasMinted[from] = false; // Allow the sender to receive another NFT in the future
+        }
+
+        if (to != address(0)) {
+            require(!_hasMinted[to], "KingOfHill: Each address can hold only one NFT");
+            _hasMinted[to] = true; // Mark the recipient as having an NFT
+        }
     }
 }
