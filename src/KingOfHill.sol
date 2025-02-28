ecbf83b7335055466876afaa8bf55b60b0722844
mike <jfkz@users.noreply.github.com>
2025-02-28 02:04:31 +0100

    feat: first working version

--- src/KingOfHill.sol
+++ src/KingOfHill.sol
@@ -1,15 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts@v4.9.3/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts@4.9.3/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract KingOfHill is ERC721, Ownable {
     uint256 private _tokenIdCounter;
     mapping(address => bool) private _hasMinted; // Tracks if an address has an NFT
 
-    // Pass the token name and symbol to the ERC721 constructor
-    constructor() ERC721("KingOfHill", "KOH") Ownable() {}
+    constructor() ERC721("KingOfHill", "KOH") Ownable(msg.sender) {}
 
     function safeMint(address to) public onlyOwner {
         require(!_hasMinted[to], "KingOfHill: Each address can hold only one NFT");
@@ -20,22 +19,16 @@ contract KingOfHill is ERC721, Ownable {
         _safeMint(to, tokenId);
     }
 
-    // Override _beforeTokenTransfer to enforce the "one NFT per wallet" rule
-    function _beforeTokenTransfer(
+    // Override transferFrom to enforce the "one NFT per wallet" rule
+    function transferFrom(
         address from,
         address to,
         uint256 tokenId
-    ) internal virtual override {
-        super._beforeTokenTransfer(from, to, tokenId);
+    ) public virtual override {
+        require(!_hasMinted[to], "KingOfHill: Each address can hold only one NFT");
+        _hasMinted[from] = false; // Allow the sender to receive another NFT in the future
+        _hasMinted[to] = true; // Mark the recipient as having an NFT
 
-        // Skip check for minting (from == address(0)) and burning (to == address(0))
-        if (from != address(0)) {
-            _hasMinted[from] = false; // Allow the sender to receive another NFT in the future
-        }
-
-        if (to != address(0)) {
-            require(!_hasMinted[to], "KingOfHill: Each address can hold only one NFT");
-            _hasMinted[to] = true; // Mark the recipient as having an NFT
-        }
+        super.transferFrom(from, to, tokenId);
     }
 }
--- test/KingOfHill.t.sol
+++ test/KingOfHill.t.sol
@@ -7,15 +7,77 @@ import "../src/KingOfHill.sol";
 contract KingOfHillTest is Test {
     KingOfHill private nft;
     address private owner = address(0x123);
-    address private user = address(0x456);
+    address private user1 = address(0x456);
+    address private user2 = address(0x789);
 
     function setUp() public {
         nft = new KingOfHill();
     }
 
+    // Test minting an NFT
     function testMint() public {
         vm.prank(owner);
-        nft.safeMint(user);
-        assertEq(nft.ownerOf(0), user);
+        nft.safeMint(user1);
+        assertEq(nft.ownerOf(0), user1);
+    }
+
+    // Test that a user cannot mint more than one NFT
+    function testCannotMintTwice() public {
+        vm.prank(owner);
+        nft.safeMint(user1);
+
+        vm.prank(owner);
+        vm.expectRevert("KingOfHill: Each address can hold only one NFT");
+        nft.safeMint(user1);
+    }
+
+    // Test that an NFT cannot be transferred to a wallet that already has an NFT
+    function testCannotTransferToWalletWithNFT() public {
+        vm.prank(owner);
+        nft.safeMint(user1);
+
+        vm.prank(owner);
+        nft.safeMint(user2);
+
+        vm.prank(user1);
+        vm.expectRevert("KingOfHill: Each address can hold only one NFT");
+        nft.transferFrom(user1, user2, 0);
+    }
+
+    // Test that an NFT can be transferred to a wallet that does not have an NFT
+    function testCanTransferToWalletWithoutNFT() public {
+        vm.prank(owner);
+        nft.safeMint(user1);
+
+        vm.prank(user1);
+        nft.transferFrom(user1, user2, 0);
+
+        assertEq(nft.ownerOf(0), user2);
+    }
+
+    // Test that safeTransferFrom works and enforces the "one NFT per wallet" rule
+    function testSafeTransferFrom() public {
+        vm.prank(owner);
+        nft.safeMint(user1);
+
+        vm.prank(owner);
+        nft.safeMint(user2);
+
+        vm.prank(user1);
+        vm.expectRevert("KingOfHill: Each address can hold only one NFT");
+        nft.safeTransferFrom(user1, user2, 0);
+    }
+
+    // Test that safeTransferFrom with data works and enforces the "one NFT per wallet" rule
+    function testSafeTransferFromWithData() public {
+        vm.prank(owner);
+        nft.safeMint(user1);
+
+        vm.prank(owner);
+        nft.safeMint(user2);
+
+        vm.prank(user1);
+        vm.expectRevert("KingOfHill: Each address can hold only one NFT");
+        nft.safeTransferFrom(user1, user2, 0, "");
     }
 }
